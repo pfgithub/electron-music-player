7e3266dc587da91465492548089e6b059fa9a9ca
pfg <omegariisthebest@gmail.com>
2019-08-08 00:18:41 -0700

    CreateElement -> qdom

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,4 @@
-import { el } from './qdom'
-
-function $scss(data: TemplateStringsArray) {
-	const styleValue = data[0]
-	const styleElem = document.createElement('style')
-	styleElem.appendChild(document.createTextNode(styleValue))
-	document.head.appendChild(styleElem)
-}
+import { $scss, el } from './qdom'
 
 $scss`
 @font-face {
--- src/qdom.ts
+++ src/qdom.ts
@@ -1,13 +1,47 @@
-export type ElementResolveable = Node | string | ElementResolveableArray
+export function $scss(data: TemplateStringsArray) {
+	const styleValue = data[0]
+	const styleElem = el.style(styleValue)
+	document.head.appendChild(styleElem)
+}
+
+export type ElementResolveable =
+	| Node
+	| string
+	| ElementResolveableArray
+	| null
+	| undefined
+	| false
 export interface ElementResolveableArray extends Array<ElementResolveable> {} // avoid circular reference
 
 export class TextBind {
 	constructor(_v: string) {}
 }
 
-export type ElementAttributes = {
-	[key: string]: string | TextBind
+export type BindAttributes<ElementType extends HTMLElement> = {
+	[event in keyof HTMLElementEventMap]: (
+		this: ElementType,
+		ev: HTMLElementEventMap[event],
+	) => void
 }
+
+export type ElementAttributes<ElementType extends HTMLElement> = {
+	[key: string]: string | TextBind | undefined | BindAttributes<ElementType>
+	// $: BindAttributes<ElementType>;
+	// $capture: BindAttributes<ElementType>;
+}
+/*
+If we weren't restricted by typescript, the syntax would be
+
+attribute: "value"
+$click: (e) => {}
+$capture_click: (e) => {}
+
+but we instead must:
+
+attribute: "value"
+$: {click: (e) => {}}
+$capture: {click: (e) => {}}
+*/
 
 export function tb(
 	literals: TemplateStringsArray,
@@ -33,11 +67,14 @@ export function resolveElement(elem: ElementResolveable): Node[] {
 		// WARNING: 2019 feature that could be replicated easily with
 		// .reduce((t, e) => (t.push(...e), t), [])
 	}
+	if (elem == null || elem === false) {
+		return []
+	}
 	return [elem]
 }
 
 export type ElementCreator<K extends keyof HTMLElementTagNameMap> = (
-	params: ElementAttributes | ElementResolveable,
+	params: ElementAttributes<HTMLElementTagNameMap[K]> | ElementResolveable,
 	...children: ElementResolveable[]
 ) => HTMLElementTagNameMap[K]
 
@@ -45,24 +82,57 @@ export function elementCreator<K extends keyof HTMLElementTagNameMap>(
 	key: K,
 ): ElementCreator<K> {
 	return (
-		params: ElementAttributes | ElementResolveable,
+		params:
+			| ElementAttributes<HTMLElementTagNameMap[K]>
+			| ElementResolveable,
 		...children: ElementResolveable[]
 	) => {
 		if (
-			params instanceof HTMLElement ||
+			params instanceof Node ||
 			typeof params === 'string' ||
-			params instanceof Text ||
-			Array.isArray(params)
+			Array.isArray(params) ||
+			params == null ||
+			params === false
 		) {
 			children.push(params)
 			params = {}
 		}
 		let el = document.createElement(key)
+		if (params.$) {
+			;(<(keyof HTMLElementEventMap)[]>Object.keys(params.$)).forEach(
+				eventName => {
+					let attrValue = (<
+						ElementAttributes<HTMLElementTagNameMap[K]>
+					>params).$![eventName] // oh no what have we done
+					el.addEventListener(eventName, <any>attrValue)
+				},
+			)
+			delete params.$
+		}
+		if (params.$capture) {
+			;(<(keyof HTMLElementEventMap)[]>(
+				Object.keys(params.$capture)
+			)).forEach(eventName => {
+				let attrValue = (<ElementAttributes<HTMLElementTagNameMap[K]>>(
+					params
+				)).$capture![eventName] // oh no what have we done
+				el.addEventListener(eventName, <any>attrValue, {
+					capture: true,
+				})
+			})
+			delete params.$capture
+		}
 		Object.keys(params).forEach(attrName => {
-			let attrValue = (<ElementAttributes>params)[attrName]
+			let attrValue = (<ElementAttributes<HTMLElementTagNameMap[K]>>(
+				params
+			))[attrName]
+			if (attrValue == null) {
+				el.removeAttribute(attrName)
+				return
+			}
 			el.setAttribute(
 				attrName,
-				attrValue instanceof TextBind ? 'textbindtodo' : attrValue,
+				attrValue instanceof TextBind ? 'textbindtodo' : `${attrValue}`,
 			)
 		})
 		children.forEach(child =>
